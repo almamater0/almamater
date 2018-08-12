06fbff6c72f2efe649df52a6301782ea9a516863
almamater0 <almamater.contacto@gmail.com>
2018-08-12 18:50:31 +0200

    prueba

--- prueba/views.py
+++ prueba/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 def prueba(request):
-
+    if request.method=="GET":
+        try:
+            prueba = Prueba.objects.get(username="prueba")
+        except:
+            prueba = "fallo."
+    context = {'prueba': prueba }
     return render(request, 'prueba.html')
