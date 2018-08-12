5b4b39c58c687ca213f470847e3c5b1aa9453062
almamater0 <almamater.contacto@gmail.com>
2018-08-12 19:13:25 +0200

    prueba

--- prueba/views.py
+++ prueba/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
 from prueba.models import p
 
-def prue(request):
-    if request.method=="GET":
-        try:
-            prueba = p.objects.values_list("texto", flat=True).get(username="prueba")
-        except:
-            prueba = "fallo."
+def prueba(request):
+    prueba = p.objects.values_list("texto", flat=True).get(username="prueba")
     context = {'prueba': prueba }
-    return render(request, 'prueba.html')
+    return render(request, 'prueba.html', context)
