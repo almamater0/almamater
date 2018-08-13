be15828c02edcfd1ced96ff6a0a8e1da75bf194a
almamater0 <almamater.contacto@gmail.com>
2018-08-13 14:36:56 +0200

    prueba

--- prueba/views.py
+++ prueba/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from prueba.models import p
 
-def prueba(request):
+def prue(request):
     prueba = p.objects.values_list("texto", flat=True).get(username="prueba")
     context = {'prueba': prueba }
     return render(request, 'prueba.html', context)
